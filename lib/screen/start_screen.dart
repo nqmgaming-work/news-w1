4dc12d62d952a7f21ca6e8aa19d1c76361043385
Nguyen Quang Minh <nguyenquangminh623@gmail.com>
2024-07-17 11:02:13 +0700

    feat: Add gradient background to StartScreen
    This commit adds a gradient background to the StartScreen widget.
    
    - Replaced the solid orange background with a linear gradient
     from blue to purple.
    - Added a white border to the "Start Quiz" button for better visibility against the gradient background.

--- lib/screen/start_screen.dart
+++ lib/screen/start_screen.dart
@@ -9,58 +9,68 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.orange,
-        body: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 100,
+        body: Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blue, Colors.purple])),
+      child: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Image.asset(
+                "assets/images/quiz-logo.png",
+                width: 200,
+                scale: 0.5,
+                alignment: Alignment.center,
               ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Image.asset(
-                  "assets/images/quiz-logo.png",
-                  width: 200,
-                  scale: 0.5,
-                  alignment: Alignment.center,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                "Learn Flutter the fun way!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18 /**/,
-                    fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              OutlinedButton(
-                  onPressed: () {
-                    logger.i("Button pressed");
-                  },
-                  style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                  child: const Text(
-                    "Start Quiz",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18,
-                        shadows: [
-                          Shadow(
-                            color: Colors.black,
-                            blurRadius: 2.0,
-                            offset: Offset(3, 3),
-                          )
-                        ]),
-                  )),
-            ],
-          ),
-        ));
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Learn Flutter the fun way!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18 /**/,
+                  fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+                onPressed: () {
+                  logger.i("Button pressed");
+                },
+                style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      width: 2.0,
+                      color: Colors.white,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                child: const Text(
+                  "Start Quiz",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      shadows: [
+                        Shadow(
+                          color: Colors.black,
+                          blurRadius: 2.0,
+                          offset: Offset(3, 3),
+                        )
+                      ]),
+                )),
+          ],
+        ),
+      ),
+    ));
   }
 }

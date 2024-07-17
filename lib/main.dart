4e7225e39829efde8798b74c0a6acbe461e698f4
Nguyen Quang Minh <nguyenquangminh623@gmail.com>
2024-07-17 10:47:06 +0700

    feat: Add start screen
    This commit adds a start screen to the application.
    
    - Created a StartScreen widget with a logo, title, and start button
    .
    - Added a logger for debugging purposes.
    - Updated the main.dart file to display the StartScreen.
    - Removed unused assets and dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,9 @@
-import 'package:first_pj/map_screen.dart';
+import 'package:first_pj/screen/start_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'gradient_container.dart';
 
-Future main() async {
-  await dotenv.load(fileName: ".env");
-  runApp( MaterialApp(
-    home:MapScreen(),
+void main() {
+  runApp(const MaterialApp(
+    home: StartScreen(),
   ));
 }
 
-class NewWidget extends StatelessWidget {
-  const NewWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          MapScreen(),
-          // GradientContainer([
-          //   Colors.blue,
-          //   Colors.green,
-          // ]),
-        ],
-      ),
-    );
-  }
-}
---
+++ lib/screen/start_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../util/logger.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.orange,
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Image.asset(
+                  "assets/images/quiz-logo.png",
+                  width: 200,
+                  scale: 0.5,
+                  alignment: Alignment.center,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Learn Flutter the fun way!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18 /**/,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              OutlinedButton(
+                  onPressed: () {
+                    logger.i("Button pressed");
+                  },
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  child: const Text(
+                    "Start Quiz",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black,
+                            blurRadius: 2.0,
+                            offset: Offset(3, 3),
+                          )
+                        ]),
+                  )),
+            ],
+          ),
+        ));
+  }
+}

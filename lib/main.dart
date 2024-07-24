7cc7d49e333428fc5fc0031caa64c72463721f1c
Nguyen Quang Minh <nguyenquangminh623@gmail.com>
2024-07-24 12:04:08 +0700

    feat: Custom paint

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,60 @@
-import 'package:first_pj/map_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'gradient_container.dart';
 
-Future main() async {
-  await dotenv.load(fileName: ".env");
-  runApp( MaterialApp(
-    home:MapScreen(),
-  ));
+class DigitalClockPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final textStyle = TextStyle(
+      color: Colors.red,
+      fontSize: 60,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'digital-7', // Sử dụng font đồng hồ kỹ thuật số
+    );
+
+    final textSpan = TextSpan(
+      text: '12:34',
+      style: textStyle,
+    );
+
+    final textPainter = TextPainter(
+      text: textSpan,
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+
+    final offset = Offset((size.width - textPainter.width) / 2, (size.height - textPainter.height) / 2);
+    textPainter.paint(canvas, offset);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
 }
 
-class NewWidget extends StatelessWidget {
-  const NewWidget({
-    super.key,
-  });
-
+class DigitalClock extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          MapScreen(),
-          // GradientContainer([
-          //   Colors.blue,
-          //   Colors.green,
-          // ]),
-        ],
+      appBar: AppBar(
+        title: Text('Digital Clock Font Example'),
+      ),
+      body: Center(
+        child: CustomPaint(
+          size: Size(300, 300),
+          painter: DigitalClockPainter(),
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: DigitalClock(),
+  ));
 }

34278bd737a88f76d3df882493e84cbff5707a64
Nguyen Quang Minh <nguyenquangminh623@gmail.com>
2024-07-25 10:17:50 +0700

    feat: Add assets and font
    This commit adds image and font assets to the project and defines constants for them.
    
    - Added image assets for icons and empty
     states.
    - Added font assets for Nunito font family.
    - Created ImageConstants and FontConstants classes to store asset paths.
    - Updated pubspec.yaml to include the new assets.
    - Removed previous unused assets and dependencies.
    - Updated main.dart to a simple hello world app.

---
+++ lib/core/image_constants.dart
@@ -0,0 +1,13 @@
+class ImageConstants {
+  static const String emptyNote = 'assets/images/empty_note.png';
+  static const String emptySearch = 'assets/images/empty_search.png';
+
+  static const String icAdd = 'assets/images/ic_add.png';
+  static const String icArrowBack = 'assets/images/ic_arrow_back.png';
+  static const String icClose = 'assets/images/ic_close.png';
+  static const String icInfo = 'assets/images/ic_info.png';
+  static const String icSave = 'assets/images/ic_save.png';
+  static const String icSearch = 'assets/images/ic_search.png';
+  static const String icVisibility = 'assets/images/ic_visibility.png';
+  static const String icDelete = 'assets/images/ic_delete.png';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,13 @@
-import 'package:first_pj/map_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'gradient_container.dart';
 
-Future main() async {
-  await dotenv.load(fileName: ".env");
-  runApp( MaterialApp(
-    home:MapScreen(),
-  ));
-}
-
-class NewWidget extends StatelessWidget {
-  const NewWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          MapScreen(),
-          // GradientContainer([
-          //   Colors.blue,
-          //   Colors.green,
-          // ]),
-        ],
-      ),
-    );
-  }
+void main() async {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('Hello World'),
+    ),
+    body: const Center(
+      child: Text('Hello World'),
+    ),
+  )));
 }
